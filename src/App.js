83d5108da1d9f1f13e8cb5b6e908f5ee418243d3
Aaron VanEtveldt <avanetveldt@lightkeeper.com>
2021-02-03 09:42:54 -0500

    some cleanup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import Header from './components/header'
-import NewsItem from './components/news-item';
 import NewsList from './components/news-list';
 import Profile from './components/profile';
 
